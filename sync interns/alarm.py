c348c7c2a7d2944f5b0de08fb7ca333462913db3
cherrypally yashwanth <yashwanthnetha6565@gmail.com>
2023-05-30 19:32:38 +0530

    init

---
+++ sync interns/alarm.py
@@ -0,0 +1,48 @@
+import datetime
+
+from playsound import playsound
+
+alarmHour = int(input("enter Hour:"))
+
+alarmMin = int(input("enter Minutes:"))
+
+
+alarmAm = input("am/pm:")
+
+
+
+
+
+
+if alarmAm=="pm":
+    
+    
+    
+    
+    
+    
+    
+    
+    alarmHour+=12
+
+
+
+
+
+
+
+
+
+while True:
+    
+    
+    if alarmHour==datetime.datetime.now().hour and alarmMin==datetime.datetime.now().minute:
+        
+        
+        
+        
+        
+        
+        print("playing...")
+        break
+    
